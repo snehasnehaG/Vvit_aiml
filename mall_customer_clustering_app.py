a78f51a45542fdf492a46f94d8abf957246f6195
snehasnehaG <snehasneha5497@gmail.com>
2026-04-09 12:21:26 +0530

    Update mall_customer_clustering_app.py

--- mall_customer_clustering_app.py
+++ mall_customer_clustering_app.py
@@ -1,4 +1,5 @@
 pip install streamlit pandas matplotlib scikit-learn
+pip install streamlit pandas matplotlib scikit-learn
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
