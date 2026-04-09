b9fbe0edd01400f73f89b5c69a1aff9de9c8ad6d
snehasnehaG <snehasneha5497@gmail.com>
2026-04-09 11:44:03 +0530

    Create mall_customer_clustering_app.py

---
+++ mall_customer_clustering_app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Load model
+with open("kmeans_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+st.title("🛍️ Mall Customer Segmentation App")
+
+st.write("Enter customer details to find their segment")
+
+# User inputs
+income = st.number_input("Annual Income (k$)", min_value=0)
+spending = st.number_input("Spending Score (1-100)", min_value=0, max_value=100)
+
+# Predict
+if st.button("Find Customer Segment"):
+    data = np.array([[income, spending]])
+    cluster = model.predict(data)[0]
+
+    st.success(f"Cluster: {cluster}")
+
+    # Meaning of cluster
+    if cluster == 0:
+        st.info("Low Income, Low Spending")
+    elif cluster == 1:
+        st.info("High Income, High Spending (Premium Customers)")
+    elif cluster == 2:
+        st.info("High Income, Low Spending (Target Customers 🎯)")
+    elif cluster == 3:
+        st.info("Low Income, High Spending")
+    else:
+        st.info("Average Customers")
